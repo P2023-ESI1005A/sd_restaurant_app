1f5ef8d21757487a06b358c593f550b47d1eade5
Edgar Vallejo Curti <45008989+BamCurti@users.noreply.github.com>
2023-02-18 13:05:09 -0600

    Revert "Feature 2"

--- mesa/mesa.py
+++
@@ -1,8 +0,0 @@
-from mesero.mesero import Mesero
-
-
-class Mesa(Mesero):
-    def __init__(self, num: int, capacidad: int, mesero: Mesero) -> None:
-        self.num:int =  num
-        self.capacidad:int = capacidad
-        self.empleado_asignado:Mesero = mesero.nombre
